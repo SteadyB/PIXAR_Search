d6ee859dc48877b6b151164f54c5aca1401fff62
SteadyB_Benjamin <benjamin.berthier.mail@gmail.com>
2018-03-12 07:58:42 +0100

    adding search items

--- page.php
+++ page.php
@@ -1,12 +1,11 @@
   <?php
 $_GET[]
 
+$findNemo = [Nemo,Dory,Marlin,Gill,Bloat,Peach,Gurgle,Bubbles,Jaques,Nigel,Crush,Coral,Squirt,Mr Ray]
+$findDory = [Nemo,Dory,Marlin,Hank,Destiny,Bailey,Jenny,Charlie,Fluke,Rudder,Mr Ray]
+$toyStory = [Buzz,Zurg,Woody,ZigZag,Mr Potato Head,Slinky Dog,Rex,Hamm,Bo peep,Andy,Sid,Ms Davis,Sergeant,Hannah,Aliens,Mr Spell,Lenny]
+$wallE = [Wall-E,EVE,Mary,Shelby Forthright,AUTO]
+$monster1 = [Sulivan,Mike,Boo,Randall,Waternoose,Celia]
 $all aray_merge($findNemo, $findDory, $toyStory, $wallE, $monster1)
-$findNemo[]
-$findDory[]
-$toyStory[]
-$wallE[]
-$monster1[]
-
 var_dump($_GET)
 ?>
